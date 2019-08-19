412149860d3a2186d1f6d6590156d66dc7e0e935
andrey <a.elyan.s@gmail.com>
2019-08-19 16:18:31 -0300

    loader of models :hammer:

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Andrey ELyan',
+    email: 'a.elyan.s@gmail.com',
+    password_hash: '123456',
+  });
+
   res.json({ message: 'Hello Guys' });
 });
 
