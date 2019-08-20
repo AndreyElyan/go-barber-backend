951a82bbd3f34ac7f2e494b8905e6a51571e41a4
andrey <a.elyan.s@gmail.com>
2019-08-20 14:05:22 -0300

    schedulle controller :hammer:

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import AppointmentController from './app/controllers/AppointmentController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ScheduleController from './app/controllers/ScheduleController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,6 +23,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
