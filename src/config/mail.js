ee60f53c0ff3db7cd64bc9e49095c93e315587de
andrey <a.elyan.s@gmail.com>
2019-08-21 08:46:35 -0300

    mail config :hammer:

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 465,
+  secure: false,
+  auth: {
+    user: '02dad155fceb79',
+    pass: '9b06cb6494d23d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
